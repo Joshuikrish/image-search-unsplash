a2cb535721ab7682b7dd77a8e118db2edffbb5b5
krtodeatho <joshuikrish@gmail.com>
2023-04-01 09:17:44 +0545

    added

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ createApp({
      async onEnter() {
       const response = await axios.get(
       "https://api.unsplash.com/search/photos?page=1&query="+this.input+"&orientation=landscape&collections=1451037&content_filter=high&aspect_ratio=1.5:1",{
-        headers: { Authorization: "Client-ID Yme6ZcumIXpWryQ0DPc249CE0ua2Mxh66Y-4W2gPAAc", },
+        headers: { Authorization: "Client-ID token", }, // place your token
       }).then(response=>{
         this.images=response.data.results
       })
